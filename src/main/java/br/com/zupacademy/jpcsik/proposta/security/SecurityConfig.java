d2b40a17d0b35a1f43550b3c18168ee4cc20186a
jpcsik <joao.batista@zup.com.br>
2021-07-23 18:56:46 -0300

    Configuração final para rodar no docker compose

--- src/main/java/br/com/zupacademy/jpcsik/proposta/security/SecurityConfig.java
+++ src/main/java/br/com/zupacademy/jpcsik/proposta/security/SecurityConfig.java
@@ -1,36 +1,29 @@
 package br.com.zupacademy.jpcsik.proposta.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http
-			.authorizeRequests()
-			.antMatchers("/actuator/**").permitAll()
-			.antMatchers("/h2-console/**").permitAll()
-			.anyRequest().hasAuthority("SCOPE_proposta")
-			.and()
-			.csrf().disable()
-			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-			.and()
-			.oauth2ResourceServer().jwt();
-			
+		http.csrf().disable()
+			.authorizeRequests(authorizeRequests -> authorizeRequests
+				.antMatchers(HttpMethod.GET, "/actuator/**").permitAll()
+				.antMatchers(HttpMethod.GET, "/actuator/prometheus").permitAll()
+				.antMatchers("/h2-console/**").permitAll()
+				.anyRequest().hasAuthority("SCOPE_proposta"))
+		.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
 	}
-	
-	 @Override
-	 public void configure(WebSecurity web) throws Exception {
-	        web
-	        .ignoring()
-	        .antMatchers("/h2-console/**");
-	    }
-	 
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/h2-console/**");
+	}
+
 }
